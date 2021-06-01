69014b8e939806be27086801e1616e0a2eebb1c9
MatheusCassio0411 <matheus.cassio.mc@gmail.com>
2021-06-01 20:23:08 -0300

    Tela_Pedido

--- lib/screen/pedido.dart
+++ lib/screen/pedido.dart
@@ -1,8 +1,12 @@
+import 'package:casa_frango/model/pedido.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PedidoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final pedidos = Provider.of<List<Pedido>>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pedidos.'),
@@ -20,6 +24,27 @@ class PedidoScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: (pedidos != null)
+          ? ListView.builder(
+              itemCount: pedidos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(pedidos[index].nome),
+                  trailing: Text(pedidos[index].fone),
+                  //trailing: Text(pedidos[index].data.toString()),
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => EditProdutoScreen(pedidos[index]),
+                      ),
+                    );
+                  },
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
